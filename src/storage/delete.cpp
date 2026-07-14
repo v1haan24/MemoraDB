8fcd84ae2212173b6f43b61eb0de705088a5a907
Naman-608 <nfornamannnn@gmail.com>
2026-07-15 00:38:06 +0530

    Updated delete.cpp

--- src/storage/delete.cpp
+++ src/storage/delete.cpp
@@ -20,21 +20,30 @@ bool Table::Delete(const string& pk)
     
     uint64_t offset = history[pk].back().offset;
     fstream file("data/"+string(meta.name)+".db",ios::binary|ios::in|ios::out);
+    if(!file)
+    {
+        cerr<<"Couldn't open file!!"<<endl;
+        return false;
+    } 
 
     file.seekg(offset+sizeof(uint64_t), ios::beg);
     bool deleted;
     readBinary(file, deleted);
-    Row  row = readPayload(file);
+    Row row = readPayload(file);
 
     if(deleted)
     {
         cerr<<"Corresponding row already DELETED!!"<<endl;
+        file.close();
         return false;
     }
 
     file.seekp(0, ios::end);
-    writeHeader(file, true);
+    uint64_t new_offset=file.tellp();
+    uint64_t timestamp = writeHeader(file, true);
     writePayload(file, row);
+
+    history[pk].push_back({timestamp, new_offset});
 
     file.close();
     return true;
